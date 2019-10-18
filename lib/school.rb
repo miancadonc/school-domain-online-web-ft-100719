72483a9e89932d63f46895c8d303d2aeb67279c1
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-18 08:12:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(school_name)
+    self.name = school_name
+    self.roster = {}
+  end
+
+  def add_student(name, grade)
+    self.roster[grade] = [] if self.roster[grade] == nil
+    self.roster[grade] << name
+  end
+  
+  def grade(grade)
+    self.roster[grade]
+  end
+  
+  def sort
+    self.roster.values.collect{|grade|grade.sort!}
+    self.roster
+  end
+  
+end
